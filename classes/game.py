518e5770d2575aca3510ffd10cb3e48e3acc6184
Elyashiv <Elyashiv2468@gmail.com>
2021-08-17 14:37:08 +0300

    Added properties

--- classes/game.py
+++ classes/game.py
@@ -11,6 +11,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class Person:
     def __init__(self, hp, mp, atk, df, magic):
         self.maxhp = hp
@@ -34,3 +35,39 @@ class Person:
     def take_damage(self, dmg):
         self.hp -= dmg
         self.hp = max(0, self.hp)
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+        self.mp = max(0, self.mp)
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        print("Action")
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print("Magic")
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
+            i += 1
